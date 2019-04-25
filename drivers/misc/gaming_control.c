8b4957e70359b32061be3f081af46fe940a8632e
Diep Quynh <remilia.1505@gmail.com>
2019-04-26 01:39:21 +0700

    gaming_control: Adapt PM QoS implementation
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/misc/gaming_control.c
+++ drivers/misc/gaming_control.c
@@ -23,6 +23,7 @@
 #include <linux/mm.h>
 #include <linux/slab.h>
 #include <linux/gaming_control.h>
+#include <linux/pm_qos.h>
 
 #define GAME_LIST_LENGTH 1024
 #define NUM_SUPPORTED_RUNNING_GAMES 20
@@ -30,12 +31,36 @@
 
 #define TASK_STARTED 1
 
+/* PM QoS implementation */
+struct pm_qos_request gaming_control_min_mif_qos;
+struct pm_qos_request gaming_control_min_big_qos;
+struct pm_qos_request gaming_control_max_big_qos;
+struct pm_qos_request gaming_control_max_little_qos;
+static unsigned int min_mif_freq = 1794000;
+static unsigned int max_little_freq = 1456000;
+static unsigned int min_big_freq = 1703000;
+static unsigned int max_big_freq = 2002000;
+
 char games_list[GAME_LIST_LENGTH] = {0};
 int games_pid[NUM_SUPPORTED_RUNNING_GAMES] = {
 	[0 ... (NUM_SUPPORTED_RUNNING_GAMES - 1)] = -1
 };
 static int nr_running_games = 0;
-int gaming_mode;
+
+static void set_gaming_mode(int mode)
+{
+	if (mode == 0) {
+		pm_qos_update_request(&gaming_control_min_mif_qos, PM_QOS_BUS_THROUGHPUT_DEFAULT_VALUE);
+		pm_qos_update_request(&gaming_control_max_little_qos, PM_QOS_CLUSTER0_FREQ_MAX_DEFAULT_VALUE);
+		pm_qos_update_request(&gaming_control_min_big_qos, PM_QOS_CLUSTER1_FREQ_MIN_DEFAULT_VALUE);
+		pm_qos_update_request(&gaming_control_max_big_qos, PM_QOS_CLUSTER1_FREQ_MAX_DEFAULT_VALUE);
+	} else if (mode == 1) {
+		pm_qos_update_request(&gaming_control_min_mif_qos, min_mif_freq);
+		pm_qos_update_request(&gaming_control_max_little_qos, max_little_freq);
+		pm_qos_update_request(&gaming_control_min_big_qos, min_big_freq);
+		pm_qos_update_request(&gaming_control_max_big_qos, max_big_freq);
+	}
+}
 
 static void store_game_pid(int pid)
 {
@@ -63,7 +88,7 @@ static void clear_dead_pids(void)
 
 	/* If there's no running games, turn off game mode */
 	if (nr_running_games == 0)
-		gaming_mode = 0;
+		set_gaming_mode(0);
 }
 
 static int check_for_games(struct task_struct *tsk)
@@ -119,13 +144,13 @@ void game_option(struct task_struct *tsk, enum game_opts opts)
 
 		store_game_pid(tsk->pid);
 		tsk->app_state = TASK_STARTED;
-		gaming_mode = 1;
+		set_gaming_mode(1);
 		break;
 	case GAME_RUNNING:
-		gaming_mode = 1;
+		set_gaming_mode(1);
 		break;
 	case GAME_PAUSE:
-		gaming_mode = 0;
+		set_gaming_mode(0);
 		break;
 	default:
 		break;
@@ -139,7 +164,6 @@ static ssize_t game_packages_show(struct kobject *kobj,
 	return sprintf(buf, "%s\n", games_list);
 }
 
-
 /* Store added games list */
 static ssize_t game_packages_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
@@ -152,21 +176,68 @@ static ssize_t game_packages_store(struct kobject *kobj,
 	return count;
 }
 
+/* Show maximum freq */
+#define show_freq(type)						\
+static ssize_t type##_show(struct kobject *kobj,		\
+		struct kobj_attribute *attr, char *buf)		\
+{								\
+	return sprintf(buf, "%u\n", type);			\
+}								\
+
+show_freq(min_mif_freq);
+show_freq(max_little_freq);
+show_freq(min_big_freq);
+show_freq(max_big_freq);
+
+/* Store maximum freq */
+#define store_freq(type)							\
+static ssize_t type##_store(struct kobject *kobj,				\
+		struct kobj_attribute *attr, const char *buf, size_t count)	\
+{										\
+	unsigned int freq;							\
+										\
+	sscanf(buf, "%u\n", &freq);						\
+	type = freq;								\
+										\
+	return count;								\
+}										\
+
+store_freq(min_mif_freq);
+store_freq(max_little_freq);
+store_freq(min_big_freq);
+store_freq(max_big_freq);
+
 static ssize_t version_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%s\n", GAMING_CONTROL_VERSION);
 }
 
-static struct kobj_attribute game_packages_attribute = 
+static struct kobj_attribute game_packages_attribute =
 	__ATTR(game_packages, 0644, game_packages_show, game_packages_store);
 
-static struct kobj_attribute version_attribute = 
+static struct kobj_attribute version_attribute =
 	__ATTR(version, 0444, version_show, NULL);
+
+static struct kobj_attribute min_mif_freq_attribute =
+	__ATTR(min_mif, 0644, min_mif_freq_show, min_mif_freq_store);
+
+static struct kobj_attribute max_little_freq_attribute =
+	__ATTR(little_freq_max, 0644, max_little_freq_show, max_little_freq_store);
+
+static struct kobj_attribute min_big_freq_attribute =
+	__ATTR(big_freq_min, 0644, min_big_freq_show, min_big_freq_store);
+
+static struct kobj_attribute max_big_freq_attribute =
+	__ATTR(big_freq_max, 0644, max_big_freq_show, max_big_freq_store);
 
 static struct attribute *gaming_control_attributes[] = {
 	&game_packages_attribute.attr,
 	&version_attribute.attr,
+	&min_mif_freq_attribute.attr,
+	&max_little_freq_attribute.attr,
+	&min_big_freq_attribute.attr,
+	&max_big_freq_attribute.attr,
 	NULL
 };
 
@@ -180,16 +251,21 @@ static int gaming_control_init(void)
 {
 	int sysfs_result;
 
+	pm_qos_add_request(&gaming_control_min_mif_qos, PM_QOS_BUS_THROUGHPUT, PM_QOS_BUS_THROUGHPUT_DEFAULT_VALUE);
+	pm_qos_add_request(&gaming_control_max_little_qos, PM_QOS_CLUSTER0_FREQ_MAX, PM_QOS_CLUSTER0_FREQ_MAX_DEFAULT_VALUE);
+	pm_qos_add_request(&gaming_control_min_big_qos, PM_QOS_CLUSTER1_FREQ_MIN, PM_QOS_CLUSTER1_FREQ_MIN_DEFAULT_VALUE);
+	pm_qos_add_request(&gaming_control_max_big_qos, PM_QOS_CLUSTER1_FREQ_MAX, PM_QOS_CLUSTER1_FREQ_MAX_DEFAULT_VALUE);
+	
 	gaming_control_kobj = kobject_create_and_add("gaming_control", kernel_kobj);
 	if (!gaming_control_kobj) {
 		pr_err("%s gaming_control kobject create failed!\n", __FUNCTION__);
 		return -ENOMEM;
-        }
+	}
 
 	sysfs_result = sysfs_create_group(gaming_control_kobj,
 			&gaming_control_control_group);
 
-        if (sysfs_result) {
+	if (sysfs_result) {
 		pr_info("%s gaming_control sysfs create failed!\n", __FUNCTION__);
 		kobject_put(gaming_control_kobj);
 	}
@@ -200,6 +276,11 @@ static int gaming_control_init(void)
 
 static void gaming_control_exit(void)
 {
+	pm_qos_remove_request(&gaming_control_min_mif_qos);
+	pm_qos_remove_request(&gaming_control_max_little_qos);
+	pm_qos_remove_request(&gaming_control_min_big_qos);
+	pm_qos_remove_request(&gaming_control_max_big_qos);
+
 	if (gaming_control_kobj != NULL)
 		kobject_put(gaming_control_kobj);
 }
