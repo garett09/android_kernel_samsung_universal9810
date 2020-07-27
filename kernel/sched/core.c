4c43e4fabe70276c28a9285935c81cc2ce9a88dd
Diep Quynh <remilia.1505@gmail.com>
2020-07-28 00:51:52 +0700

    sched: ems: Update EMS to latest beyond2lte source
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -760,6 +760,9 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	update_rq_clock(rq);
 	if (!(flags & ENQUEUE_RESTORE))
 		sched_info_queued(rq, p);
+
+	update_cpu_active_ratio(rq, p, EMS_PART_ENQUEUE);
+
 	p->sched_class->enqueue_task(rq, p, flags);
 }
 
@@ -768,6 +771,9 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 	update_rq_clock(rq);
 	if (!(flags & DEQUEUE_SAVE))
 		sched_info_dequeued(rq, p);
+
+	update_cpu_active_ratio(rq, p, EMS_PART_DEQUEUE);
+
 	p->sched_class->dequeue_task(rq, p, flags);
 }
 
@@ -2625,6 +2631,8 @@ void wake_up_new_task(struct task_struct *p)
 	update_rq_clock(rq);
 	post_init_entity_util_avg(&p->se);
 
+	update_cpu_active_ratio(rq, p, EMS_PART_WAKEUP_NEW);
+
 	walt_mark_task_starting(p);
 
 	activate_task(rq, p, ENQUEUE_WAKEUP_NEW);
@@ -3159,6 +3167,7 @@ void scheduler_tick(void)
 	sched_clock_tick();
 
 	raw_spin_lock(&rq->lock);
+	set_part_period_start(rq);
 	walt_set_window_start(rq);
 	walt_update_task_ravg(rq->curr, rq, TASK_UPDATE,
 			walt_ktime_clock(), 0);
@@ -7962,6 +7971,11 @@ void __init sched_init(void)
 
 		INIT_LIST_HEAD(&rq->cfs_tasks);
 
+#ifdef CONFIG_SCHED_EMS
+		INIT_LIST_HEAD(&rq->sse_cfs_tasks);
+		INIT_LIST_HEAD(&rq->uss_cfs_tasks);
+#endif
+
 		rq_attach_root(rq, &def_root_domain);
 #ifdef CONFIG_NO_HZ_COMMON
 		rq->last_load_update_tick = jiffies;
@@ -7976,8 +7990,6 @@ void __init sched_init(void)
 	}
 
 	set_load_weight(&init_task);
-
-	alloc_bands();
 
 	/*
 	 * The boot idle thread does lazy MMU switching as well:
@@ -8006,6 +8018,8 @@ void __init sched_init(void)
 	init_sched_fair_class();
 
 	init_schedstats();
+
+	init_ems();
 
 	scheduler_running = 1;
 }
