6658edfa44b581fcf59fc92bdcc97bf3535276bd
Diep Quynh <remilia.1505@gmail.com>
2021-04-01 10:07:23 -0400

    gpu: mali: bv_r26p0: Limit big cluster frequency only on high GPU util
    This is a workaround for gaming scenairo when big cluster thermal
    went over the roof, and will not interfere most latency sensitive
    user workloads like web browsing
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/gpu/arm/bv_r26p0/platform/exynos/gpu_pmqos.c
+++ drivers/gpu/arm/bv_r26p0/platform/exynos/gpu_pmqos.c
@@ -125,11 +125,11 @@ int gpu_pm_qos_command(struct exynos_context *platform, gpu_pmqos_state state)
 		mutex_unlock(&platform->gpu_vk_boost_lock);
 #endif
 		pm_qos_update_request(&exynos5_g3d_cpu_cluster0_min_qos, platform->table[platform->step].cpu_little_min_freq); // LITTLE Min request
-		if (!platform->boost_is_enabled)
+		if (!platform->boost_is_enabled && platform->env_data.utilization > 65)
 			pm_qos_update_request(&exynos5_g3d_cpu_cluster1_max_qos, platform->table[platform->step].cpu_big_max_freq);
 #if PM_QOS_CPU_CLUSTER_NUM == 3
 		pm_qos_update_request(&exynos5_g3d_cpu_cluster1_min_qos, platform->table[platform->step].cpu_middle_min_freq); /* MIDDLE Min request */
-		if (!platform->boost_is_enabled)
+		if (!platform->boost_is_enabled && platform->env_data.utilization > 65)
 		pm_qos_update_request(&exynos5_g3d_cpu_cluster2_max_qos, platform->table[platform->step].cpu_big_max_freq); /* BIG Max request */
 #ifdef CONFIG_MALI_SUSTAINABLE_OPT
 		if (platform->sustainable.info_array[0] > 0) {
