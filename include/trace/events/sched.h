MotorolaMobilityLLC/kernel-slsi

trace: events: sched: Add event "activated_cpus" for frt
Change-Id: I97802fb9f7bd0f3102ad3686848cc73bc4855966
Signed-off-by: Youngtae Lee <yt0729.lee@samsung.com>

include/trace/events/sched.h
@@ -642,6 +642,32 @@ struct cfs_rq *__trace_sched_group_cfs_rq(struct sched_entity *se)
 /*
  * Tracepoint for logging FRT schedule activity
  */
+
+TRACE_EVENT(sched_fluid_activated_cpus,
+
+	TP_PROTO(int cpu, int util_sum, int busy_thr, unsigned int prefer_mask),
+
+	TP_ARGS(cpu, util_sum, busy_thr, prefer_mask),
+
+	TP_STRUCT__entry(
+		__field(	int,		cpu		)
+		__field(	int,		util_sum	)
+		__field(	int,		busy_thr	)
+		__field(	unsigned long,	prefer_mask	)
+	),
+
+	TP_fast_assign(
+		__entry->cpu		= cpu;
+		__entry->util_sum	= util_sum;
+		__entry->busy_thr	= busy_thr;
+		__entry->prefer_mask	= prefer_mask;
+	),
+
+	TP_printk("cpu=%d util_sum=%d busy_thr=%d prefer_mask=%x",
+		__entry->cpu,__entry->util_sum,
+		__entry->busy_thr, __entry->prefer_mask)
+);
+
 TRACE_EVENT(sched_fluid_stat,
 
 	TP_PROTO(struct task_struct *tsk, struct sched_avg *avg, int best, char* str),

garett09/android_kernel_samsung_universal9810

include/trace/events/sched.h
@@ -590,6 +590,32 @@ TRACE_EVENT(sched_wake_idle_without_ipi,
 /*
  * Tracepoint for logging FRT schedule activity
  */
+
+TRACE_EVENT(sched_fluid_activated_cpus,
+
+	TP_PROTO(int cpu, int util_sum, int busy_thr, unsigned int prefer_mask),
+
+	TP_ARGS(cpu, util_sum, busy_thr, prefer_mask),
+
+	TP_STRUCT__entry(
+		__field(	int,		cpu		)
+		__field(	int,		util_sum	)
+		__field(	int,		busy_thr	)
+		__field(	unsigned long,	prefer_mask	)
+	),
+
+	TP_fast_assign(
+		__entry->cpu		= cpu;
+		__entry->util_sum	= util_sum;
+		__entry->busy_thr	= busy_thr;
+		__entry->prefer_mask	= prefer_mask;
+	),
+
+	TP_printk("cpu=%d util_sum=%d busy_thr=%d prefer_mask=%lu",
+		__entry->cpu,__entry->util_sum,
+		__entry->busy_thr, __entry->prefer_mask)
+);
+
 TRACE_EVENT(sched_fluid_stat,
 
 	TP_PROTO(struct task_struct *tsk, struct sched_avg *avg, int best, char* str),