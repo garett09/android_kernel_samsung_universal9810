8ae5a75b61be32fd54da5f4796f78e05c62da907
Adrian Sian <adriansian@gmail.com>
2021-07-18 01:18:19 +0800

    fs/sdfat: fix build when SDFAT_DEBUG unset

--- fs/sdfat/blkdev.c
+++ fs/sdfat/blkdev.c
@@ -96,6 +96,9 @@ s32 bdev_check_bdi_valid(struct super_block *sb)
 			fsi->prev_eio |= SDFAT_EIO_BDI;
 			sdfat_log_msg(sb, KERN_ERR, "%s: block device is "
 				"eliminated.(bdi:%p)", __func__, sb->s_bdi);
+#ifdef CONFIG_SDFAT_DEBUG
+			sdfat_debug_warn_on(1);
+#endif
 		}
 		return -ENXIO;
 	}
